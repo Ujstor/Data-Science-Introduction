f006a981e9a9407eb3748cae9117a448a802dc53
ujstor <astipan@gmail.com>
2023-07-18 00:33:11 +0200

    workingData analysis

--- 01_numpy_pandas_matplotlib/WorkingHours_data_analysis/data/combine_data.py
+++ 01_numpy_pandas_matplotlib/WorkingHours_data_analysis/data/combine_data.py
@@ -1,15 +1,22 @@
 import pandas as pd
+import os
 import glob
 
-xlsx_dir = '01_numpy_pandas_matplotlib/working_data_analysis/data/rawdata/data_form1/'
-xlsx_files = glob.glob(xlsx_dir + '*.xlsx')
+input_dir = '01_numpy_pandas_matplotlib/WorkingHours_data_analysis/data/rawdata/'
+output_dir = '01_numpy_pandas_matplotlib/WorkingHours_data_analysis/data/rawdata/'
 
-combined_data = pd.DataFrame()
+for form_dir in glob.glob(input_dir + 'data_form*'):
+    form_number = form_dir.split('_')[-1]
+    xlsx_files = glob.glob(form_dir + '/*.xlsx')
 
-for file in xlsx_files:
-    data = pd.read_excel(file)
-    combined_data = pd.concat([combined_data, data], ignore_index=True)
+    combined_data = pd.DataFrame()
 
-output_csv = '01_numpy_pandas_matplotlib/working_data_analysis/data/combined_data1.csv'
-combined_data.to_csv(output_csv, index=False)
+    for file in xlsx_files:
+        data = pd.read_excel(file)
+        combined_data = pd.concat([combined_data, data], ignore_index=True)
+
+    output_csv = os.path.join(output_dir, f'combined_data{form_number}.csv')
+    combined_data.to_csv(output_csv, index=False)
+
+    print(f"Combined data {form_number} saved to: {output_csv}")
 
